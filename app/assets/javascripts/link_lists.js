1b05c4bc0f854b43d213790d91a1d43b53de1f2f
Dave Mayo <dave_mayo@harvard.edu>
2014-10-17 12:16:02 -0400

    WIP Self Service

--- app/assets/javascripts/link_lists.js
+++ app/assets/javascripts/link_lists.js
@@ -1,6 +1,30 @@
 $(function () {
+  $(document).on('cocoon:before-remove', function (e, removed_item) {
+    $rem = $(removed_item);
+    if ($rem.hasClass('deleted')){
+      $rem.addClass('undelete');
+    }
+  });
   $(document).on('cocoon:after-remove', function (e, removed_item) {
-    $(removed_item).show();
-    $(removed_item).addClass('deleted');
+    $rem.show();
+    $rem = $(removed_item);
+    if ($rem.hasClass('undelete')) {
+      $rem.find('.links > input').remove();
+      $rem.removeClass('deleted').removeClass('undelete')
+      $rem.find('.links > a')
+        .removeClass('btn-warning')
+        .addClass('btn-danger')
+        .text('Remove');
+    }
+    else {
+      $rem.addClass('deleted');
+      $rem.find('.links > a')
+        .removeClass('btn-danger')
+        .addClass('btn-warning')
+        .text('Restore')
+    }
+  });
+  $(document).on('cocoon:after-insert', function (e, added_item) {
+    $(document).scrollTop($(document).scrollTop() + $(added_item).outerHeight());
   });
 });
