0e91546bc85db3bdf85425d4463c408a22a6bd1a
Dave Mayo <dave_mayo@harvard.edu>
2014-10-23 13:40:44 -0400

    Import now defloatifies floats.

--- app/models/link_list.rb
+++ app/models/link_list.rb
@@ -1,3 +1,16 @@
+# coding: utf-8
+
+# Monkeypatch for Roo::Excelx because we don't want any floats
+class Roo::Excelx
+  def hl_cell(row, col, sheet = nil)
+    if self.celltype(row, col, sheet) == :float
+      self.excelx_value(row, col, sheet)
+    else
+      self.cell(row, col, sheet)
+    end
+  end
+end
+
 class LinkList < ActiveRecord::Base
   has_many :links, -> { order('position ASC')}
   accepts_nested_attributes_for :links, :reject_if => :all_blank, :allow_destroy => true
@@ -7,21 +20,22 @@ class LinkList < ActiveRecord::Base
     ext_id
   end
 
-  # NOTE: Cells that can be interpreted as number values will be so interpreted - coercion to string should be forced.
+  # Converts from .xlsx format, treating all fields as strings.
+  # Note that indexing starts from 1, because spreadsheets ¯\_(ツ)_/¯
   def self.import_xlsx(excel)
     result = LinkList.new
 
-    # Find split between headers and content
-    separator = excel.find_index {|row| row[0] == 'CONTENT_LIST'} || raise(StandardError, "No CONTENT_LIST in excel")
-    rows = excel.to_a
-    (naught, url) = *rows[0]
+    # Find split between headers and content ()
+    separator = (excel.find_index {|row| row[0] == 'CONTENT_LIST'} + 1) || # array_index + 1 == spreadsheet_index
+                raise(StandardError, "No CONTENT_LIST in excel")
+    (naught, url) = excel.hl_cell(1,1), excel.hl_cell(1,2)
 
     raise StandardError, "Initial row must consist of [blank, url], not #{[naught, url]}" unless naught.blank?
     result.url = url
     result.ext_id = url.match(/\d+$/)[0] # throw exception if blank!
 
-    # process headers
-    rows[1...separator].each do |(key, content, extra)|
+    (1..separator).each do |row_i|
+      (key, content, extra) = *((1..3).map {|col_i| excel.hl_cell row_i, col_i })
       case key
       when /^continues/i
         result.continues_name = content
@@ -39,7 +53,8 @@ class LinkList < ActiveRecord::Base
     end
 
     # process content
-    result.links = rows[(separator+1)...rows.count].map do |(label, url)|
+    result.links = ((separator+1)..excel.last_row).map do |row_i|
+      (label,url) = *((1..2).map {|col_i| excel.hl_cell row_i, col_i })
       Link.new(:name => label, :url => url)
     end
 
--- test/models/link_list_test.rb
+++ test/models/link_list_test.rb
@@ -9,10 +9,14 @@ class LinkListTest < ActiveSupport::TestCase
   end
 
   test "imported xslx does not convert numbers to floats" do
+    begin
     excel = Roo::Excelx.new('test/data/HOLLIS_Links_001787337.xlsx')
     me = LinkList.import_xlsx(excel)
-    assert_instance_of String, me.links.first.name, 'should be a string'
+    assert_instance_of String, me.links.first.name
     assert_equal '1915', me.links.first.name, 'first link of this record should be 1915.'
+    rescue StandardError => e
+      binding.pry
+    end
   end
 
   test "fetches metadata" do
