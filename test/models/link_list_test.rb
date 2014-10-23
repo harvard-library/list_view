810df95e5f580e7f2ef14528baa4fb1ebdcaf59b
Dave Mayo <dave_mayo@harvard.edu>
2014-10-23 10:12:09 -0400

    Basic tests

---
+++ test/models/link_list_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class LinkListTest < ActiveSupport::TestCase
+  test "imports an xslx file" do
+    excel = Roo::Excelx.new('test/data/HOLLIS_Links_0013124263.xlsx')
+    me = LinkList.import_xlsx(excel)
+    assert_instance_of LinkList, me, "is not a linklist"
+    assert me.save, "failed to save successfully"
+  end
+
+  test "imported xslx does not convert numbers to floats" do
+    excel = Roo::Excelx.new('test/data/HOLLIS_Links_001787337.xlsx')
+    me = LinkList.import_xlsx(excel)
+    assert_instance_of String, me.links.first.name, 'should be a string'
+    assert_equal '1915', me.links.first.name, 'first link of this record should be 1915.'
+  end
+
+  test "fetches metadata" do
+    excel = Roo::Excelx.new('test/data/HOLLIS_Links_0013124263.xlsx')
+    me = LinkList.import_xlsx(excel)
+    me.fetch_metadata
+    assert me.cached_metadata, "metadata does not exist"
+  end
+end
