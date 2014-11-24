e64212d0504ebc69c08d760edc35e27a3feb4f84
Dave Mayo <dave_mayo@harvard.edu>
2014-11-24 16:27:17 -0500

    First working? pass at user password handling.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
   before_action :authenticate_login!
 
+  # Helper methods
+
+  def vet_password
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+
+    if params[:user][:password] != params[:user][:password_confirmation]
+      flash[:error] = 'Password and confirmation must match.'
+      redirect_to :back
+    end
+  end
+
+  # Collection actions
+
   def index
     @users = User.order(:id)
   end
@@ -8,6 +24,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    vet_password
+    if @user.save
+      flash[:notice] = "User sucessfully created!"
+      redirect_to @user, :action => :show
+    else
+      flash[:error] = "Failed to save user."
+      redirect_to :back
+    end
+  end
+
+  # Member actions
 
   def show
     @user = User.find(params[:id])
@@ -19,29 +50,24 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
+
+    vet_password
+
     @user.attributes = user_params
     if @user.save
-      flash.now[:notice] = "User updated sucessfully!"
-      render @user
+      flash[:notice] = "User updated sucessfully!"
+      respond_to do |f|
+        f.html { redirect_to @user, :action => :show }
+      end
     else
       flash[:error] = "Failed to update user."
       redirect_to :back
     end
   end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      flash.now[:notice] = "User sucessfully created!"
-      render @user
-    else
-      flash[:error] = "Failed to save user."
-      redirect_to :back
-    end
-  end
 
   private
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
