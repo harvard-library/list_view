6e273247808402e604fd9bf87efbe22ec5db24a7
Dave Mayo <dave_mayo@harvard.edu>
2014-12-03 10:24:33 -0500

    Style fixes, icons for edit and csv buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def glyphicon(name, at_end = true)
+    output = ''
+    output << "&nbsp;" if at_end
+    output << "<span class=\"glyphicon glyphicon-#{name}\"></span>"
+    raw output
+  end
 end
