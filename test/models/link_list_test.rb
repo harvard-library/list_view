916f0684d905c5902254871a7e384b50e3e91752
Dave Mayo <dave_mayo@harvard.edu>
2014-12-22 13:56:25 -0500

    Test updates, also made icon protocol agnostic

--- test/models/link_list_test.rb
+++ test/models/link_list_test.rb
@@ -2,26 +2,30 @@ require 'test_helper'
 
 class LinkListTest < ActiveSupport::TestCase
   test "imports an xlsx file" do
-    excel = Roo::Excelx.new('test/data/HOLLIS_Links_0013124263.xlsx')
+    filename = Dir.glob(File.join('test', 'data', '*.xlsx')).first
+    excel = Roo::Excelx.new(filename)
     me = LinkList.import_xlsx(excel)
     assert_instance_of LinkList, me, "is not a linklist"
     assert me.save, "failed to save successfully"
   end
 
   test "imported xlsx does not convert numbers to floats" do
-    begin
     excel = Roo::Excelx.new('test/data/HOLLIS_Links_001787337.xlsx')
     me = LinkList.import_xlsx(excel)
     assert_instance_of String, me.links.first.name
     assert_equal '1915', me.links.first.name, 'first link of this record should be 1915.'
-    rescue StandardError => e
-      binding.pry
-    end
+  end
+
+  test "imports a csv file" do
+    filename = Dir.glob(File.join('test', 'data', '*.csv')).first
+    me = LinkList.import_csv(CSV.read(filename))
+    assert_instance_of LinkList, me, "is not a linklist"
+    assert me.save, "failed to save successfully"
   end
 
   test "fetches metadata" do
-    excel = Roo::Excelx.new('test/data/HOLLIS_Links_0013124263.xlsx')
-    me = LinkList.import_xlsx(excel)
+    filename = Dir.glob(File.join('test', 'data', '*.csv')).first
+    me = LinkList.import_csv(CSV.read(filename))
     me.fetch_metadata
     assert me.cached_metadata, "metadata does not exist"
   end
