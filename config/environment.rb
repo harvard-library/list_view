0652159bdf84e151b66b90040dbac4561d513ec2
bobbi-SMR <bobbi_fox@harvard.edu>
2016-03-14 15:53:20 -0400

    add action_mailer.default_url_options in the correct file

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-
+#  Tell the mailer where to go
+Rails.configuration.action_mailer.default_url_options = {host: ENV['ROOT_URL']}
 # Initialize the Rails application.
 Rails.application.initialize!
